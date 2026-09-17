5a88d800f92d9e26288348a20cce872f3fa57730
Cicero <cicerodasilvatj@gmail.com>
2026-09-16 22:23:28 -0300

    Refatoração do cálculo que estava errado

--- src/main/java/com/example/services/ShippingService.java
+++ src/main/java/com/example/services/ShippingService.java
@@ -11,12 +11,12 @@ public class  ShippingService {
 
     public double shippiment(Order order){
         this.order = order;
-        if(order.getBasic() < 100.0){
-            return order.getBasic() + 20.0;
-        } else if(order.getBasic() >= 100.0 && order.getBasic() < 200.0){
-            return order.getBasic() + 12.0;
+        if (order.getBasic() < 100.0) {
+            return 20.0;
+        } else if (order.getBasic() >= 100.0 && order.getBasic() < 200.0) {
+            return 12.0;
         } else {
-            return order.getBasic();
+            return 0.0;
         }
     }
 
